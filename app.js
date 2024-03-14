470e015f9c0fc0a166002dc74d637a7852c20977
Maddy221 <student.154309@worcesterschools.net>
2024-03-14 10:48:21 -0400

    Changed photos

--- app.js
+++ app.js
@@ -18,10 +18,36 @@ function playerAttack(evt){
 }
 
 function playerHeal(evt){
+    //random heal
+    const heal = Math.floor(Math.random()*21)+10
     //increase the life points
+    lifePoints += heal;
+    document.querySelector('.player span').innerHTML = lifePoints
+    botAttack()
+
 }
 
 function botAttack(){
+  if (botPoints <= 30){
+    botHeal()
+  }
+  else{
     //damage lifePoints
+    //create random damage
+    //range 10-30
+    const dmg = Math.floor(Math.random()*21)+10
+  // damage the botPoints
+    lifePoints -= dmg;
+  //change the botPoints on screen
+    document.querySelector('.player span').innerHTML = lifePoints
+  //triggers the bot attack
     //change the lifePoints on screen
+  }
+}
+
+function botHeal(){
+  const heal = Math.floor(Math.random()*21)+10
+    //increase the life points
+    botPoints += heal;
+    document.querySelector('.player span').innerHTML = botPoints
 }
